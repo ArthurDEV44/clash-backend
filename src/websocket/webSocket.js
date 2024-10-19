2283c5aa57cdfeb316cf7805e98a451db50072c4
ArthurDEV44 <arthurjean-dev@outlook.fr>
2024-10-19 02:02:37 +0200

    Fix Prob WebSocket

--- src/websocket/webSocket.js
+++ src/websocket/webSocket.js
@@ -3,11 +3,28 @@ const playerService = require('../services/playerService');
 
 let wss;
 
+// Stockage de l'état du clash sur le serveur
+let clashState = {
+  players: [],
+  playerList: [],
+  maps: [1],
+  clashStarted: false,
+  clashFinished: false,
+  winner: null,
+  showModal: false,
+};
+
 function setupWebSocket(server) {
   wss = new WebSocket.Server({ server });
 
   wss.on('connection', (ws) => {
-    console.log('A WebSocket client connected.');
+    console.log('Un client WebSocket s\'est connecté.');
+
+    // Envoyer l'état initial au nouveau client
+    ws.send(JSON.stringify({
+      type: 'initial_state',
+      data: clashState,
+    }));
 
     ws.on('message', (message) => {
       const parsedMessage = JSON.parse(message);
@@ -15,14 +32,14 @@ function setupWebSocket(server) {
     });
 
     ws.on('close', () => {
-      console.log('A WebSocket client disconnected.');
+      console.log('Un client WebSocket s\'est déconnecté.');
     });
   });
 }
 
 function broadcastMessage(message, sender = null) {
   if (!wss) {
-    console.error('WebSocket server not initialized');
+    console.error('Serveur WebSocket non initialisé');
     return;
   }
   wss.clients.forEach((client) => {
@@ -35,40 +52,75 @@ function broadcastMessage(message, sender = null) {
 // Gestion des messages WebSocket
 function handleWebSocketMessage(message, ws) {
   switch (message.type) {
-    case 'update_player':
-      handleUpdatePlayer(message, ws);
-      break;
     case 'update_players':
-      broadcastMessage(message, ws);
+      clashState.players = message.data;
+      broadcastMessage({ type: 'update_players', data: clashState.players }, ws);
       break;
+
+    case 'update_player_list':
+      clashState.playerList = message.data;
+      broadcastMessage({ type: 'update_player_list', data: clashState.playerList }, ws);
+      break;
+
     case 'add_map':
-    case 'remove_map':
-    case 'remove_player_from_clash':
-    case 'clash_started':
-    case 'clash_finished':
-    case 'show_modal':
-      broadcastMessage(message, ws);
+      clashState.maps.push(message.newMapIndex);
+      clashState.players = message.updatedPlayers;
+      broadcastMessage({
+        type: 'add_map',
+        newMapIndex: message.newMapIndex,
+        updatedPlayers: clashState.players,
+      }, ws);
       break;
+
+    case 'remove_map':
+      clashState.maps.pop();
+      clashState.players = message.updatedPlayers;
+      broadcastMessage({
+        type: 'remove_map',
+        mapIndexToRemove: message.mapIndexToRemove,
+        updatedPlayers: clashState.players,
+      }, ws);
+      break;
+
+    case 'remove_player_from_clash':
+      clashState.players = clashState.players.filter(player => player.id !== message.playerId);
+      broadcastMessage({ type: 'remove_player_from_clash', playerId: message.playerId }, ws);
+      break;
+
+    case 'clash_started':
+      clashState.clashStarted = true;
+      broadcastMessage({ type: 'clash_started' }, ws);
+      break;
+
+    case 'clash_finished':
+      clashState.clashStarted = false;
+      clashState.clashFinished = false;
+      clashState.winner = null;
+      clashState.showModal = false;
+      clashState.players = []; // Réinitialiser les joueurs du clash
+      clashState.maps = [1]; // Réinitialiser les maps
+      broadcastMessage({ type: 'clash_finished' }, ws);
+      break;
+
+    case 'show_modal':
+      clashState.winner = message.winner;
+      clashState.showModal = true;
+      broadcastMessage({ type: 'show_modal', winner: message.winner }, ws);
+      break;
+
+    case 'new_player':
+      clashState.playerList.push(message.data);
+      broadcastMessage({ type: 'new_player', data: message.data }, ws);
+      break;
+
+    case 'delete_player_from_list':
+      clashState.playerList = clashState.playerList.filter(player => player.id !== message.playerId);
+      broadcastMessage({ type: 'delete_player_from_list', playerId: message.playerId }, ws);
+      break;
+
     default:
-      console.log('Unknown message type:', message.type);
+      console.log('Type de message inconnu :', message.type);
   }
-}
-
-// Gestion de la mise à jour d'un joueur
-function handleUpdatePlayer(message, ws) {
-  const player = message.data;
-  const playerId = player.id;
-  const name = player.name;
-
-  // Mettre à jour le joueur
-  playerService.updatePlayerMaps(playerId, player.maps)
-    .then(() => {
-      // Diffuser la mise à jour aux autres clients
-      broadcastMessage({ type: 'update_player', data: player }, ws);
-    })
-    .catch(err => {
-      console.error('Error updating player:', err);
-    });
 }
 
 module.exports = {
